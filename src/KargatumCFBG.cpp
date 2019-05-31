aa536c710673f1bc0f96d2c184e2e6b1fa3e7883
Winfidonarleyan <dowlandtop@yandex.ru>
2019-05-31 17:57:26 +0700

    Fix typo (2/2)

--- src/KargatumCFBG.cpp
+++ src/KargatumCFBG.cpp
@@ -191,6 +191,7 @@ void CFBG::SetFakeRaceAndMorph(Player* player)
     this->SetFactionForRace(player, FakeRace);
     player->SetDisplayId(FakeMorph);
     player->SetNativeDisplayId(FakeMorph);
+}
 
 void CFBG::SetFactionForRace(Player* player, uint8 Race)
 {
