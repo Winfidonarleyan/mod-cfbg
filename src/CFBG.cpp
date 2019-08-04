0fbaf3b3d1aa4dd7999b5ac593d1092bdd22a389
Winfidonarleyan <dowlandtop@yandex.ru>
2019-08-04 16:31:57 +0700

    Added support global announce (if enable in conf)

--- src/CFBG.cpp
+++ src/CFBG.cpp
@@ -11,6 +11,7 @@
 #include "BattlegroundMgr.h"
 #include "Opcodes.h"
 #include "Chat.h"
+#include "Language.h"
 
 CFBG* CFBG::instance()
 {
@@ -493,7 +494,10 @@ bool CFBG::SendMessageQueue(BattlegroundQueue* bgqueue, Battleground* bg, PvPDif
     uint32 MinPlayers = bg->GetMinPlayersPerTeam() * 2;
     uint32 qPlayers = bgqueue->GetPlayersCountInGroupsQueue(bracketId, (BattlegroundQueueGroupTypes)BG_QUEUE_CFBG);
 
-    ChatHandler(leader->GetSession()).PSendSysMessage("CFBG %s (Levels: %u - %u). Registered: %u/%u", bgName, q_min_level, q_max_level, qPlayers, MinPlayers);
+    if (sWorld->getBoolConfig(CONFIG_BATTLEGROUND_QUEUE_ANNOUNCER_PLAYERONLY))
+        ChatHandler(leader->GetSession()).PSendSysMessage("CFBG %s (Levels: %u - %u). Registered: %u/%u", bgName, q_min_level, q_max_level, qPlayers, MinPlayers);
+    else
+        sWorld->SendWorldText(LANG_BG_QUEUE_ANNOUNCE_WORLD, bgName, q_min_level, q_max_level, qPlayers, MinPlayers);
 
     return true;
 }
